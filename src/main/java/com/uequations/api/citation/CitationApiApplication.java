434e06b267d3e99ff9f2775cd87ff7554ec7d370
malkebu-lan <malkebu-lan@uequations.com>
2018-10-19 00:27:42 -0400

    initial commit

---
+++ src/main/java/com/uequations/api/citation/CitationApiApplication.java
@@ -0,0 +1,15 @@
+package com.uequations.api.citation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mensah Alkebu-Lan
+ */
+@SpringBootApplication
+public class CitationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitationApiApplication.class, args);
+    }
+}
---
+++ src/main/java/com/uequations/api/citation/config/CitationApiResourceConfig.java
@@ -0,0 +1,9 @@
+package com.uequations.api.citation.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * @author Mensah Alkebu-Lan
+ */
+public class CitationApiResourceConfig extends ResourceConfig {
+}
